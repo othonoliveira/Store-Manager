1ba5ebd8cd702c995fa79c791862b38b979e700d
Othon Oliveira <othonoliveira08@gmail.com>
2023-03-14 01:34:53 -0300

    Teste

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -2,21 +2,20 @@ const salesModel = require('../models/sales.model');
 const { validateData } = require('./validations/salesValidations');
 
 const createSale = async (saleInfo) => {
-  const { itemsSold } = saleInfo;
-  let error = await validateData(itemsSold[0]);
+  let error = await validateData(saleInfo[0]);
   if (error.type) {
     return error;
   }
-  error = await validateData(itemsSold[1]);
+  error = await validateData(saleInfo[1]);
   if (error.type) {
     return error;
   }
 
   const saleId = await salesModel.createSale();
-  await salesModel.saleProducts(saleId, itemsSold[0].productId, itemsSold[0].quantity);
-  await salesModel.saleProducts(saleId, itemsSold[1].productId, itemsSold[1].quantity);
+  await salesModel.saleProducts(saleId, saleInfo[0].productId, saleInfo[0].quantity);
+  await salesModel.saleProducts(saleId, saleInfo[1].productId, saleInfo[1].quantity);
 
-  return { type: null, message: { id: saleId, itemSold: saleInfo } };
+  return { type: null, message: { id: saleId, itemSold: [...saleInfo] } };
 };
 
 module.exports = {
